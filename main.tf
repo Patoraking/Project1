a7ef7e90adc7489a6171d759981ee19916a09856
Omorovan <patrick.omorovan@catalent.com>
2024-03-04 12:04:00 +0000

    updated the terraform codes

--- main.tf
+++ main.tf
@@ -1,3 +1,5 @@
+#Script to deploy ec2 into a subnet, within a VPC and network interface
+
 terraform {
   required_providers {
     aws = {
@@ -15,34 +17,32 @@ provider "aws" {
 
 resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.0/16"
-  region = "us-east-1"
-
      
 }
 
 resource "aws_subnet" "public_subnet" {
   vpc_id            = aws_vpc.my_vpc.id 
-  cidr_block        = "10.0.0.0/24"
+  cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
 
 
 }
 
-resource "aws_network_interface" "foo" {
+resource "aws_network_interface" "main" {
   subnet_id   = aws_subnet.public_subnet.id
-  private_ips = ["10.0.0.2"]
+  private_ips = ["10.0.1.30"]
 
   tags = {
     Name = "primary_network_interface"
   }
 }
 
-resource "aws_instance" "foo" {
-  ami           = "ami-005e54dee72cc1d00" # us-west-2
+resource "aws_instance" "linux" {
+  ami           = "ami-07d9b9ddc6cd8dd30" # us-west-2
   instance_type = "t2.micro"
 
   network_interface {
-    network_interface_id = aws_network_interface.foo.id
+    network_interface_id = aws_network_interface.main.id
     device_index         = 0
   }
 
@@ -52,13 +52,16 @@ resource "aws_instance" "foo" {
 }
 
  
-resource "aws_internet_gateway_attachment" "example" {
-  internet_gateway_id = aws_internet_gateway.example.id
-  vpc_id              = aws_vpc.example.id
+resource "aws_internet_gateway_attachment" "main" {
+  internet_gateway_id = aws_internet_gateway.main.id
+  vpc_id              =  aws_vpc.my_vpc.id
 }
 
-resource "aws_vpc" "example" {
-  cidr_block = "10.1.0.0/16"
-}
 
-resource "aws_internet_gateway" "example" {}
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "main"
+  }
+}
