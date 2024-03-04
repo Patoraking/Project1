457feedd0ebf1bf10dd744b475a8e0e0e51f5c20
Patrick Omorovan <patrickomorovan@gmail.com>
2024-03-04 16:22:36 +0000

    update

--- main.tf
+++ main.tf
@@ -15,18 +15,109 @@ provider "aws" {
 }
 
 
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0/16"
+resource "aws_vpc" "custom_vpc" {
+  cidr_block = "10.0.0.0/16"
      
 }
 
+resource "aws_internet_gateway" "custom_gw" {
+  vpc_id = aws_vpc.custom_vpc.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.custom_vpc.id
+
+  route {
+    cidr_block = "10.0.1.0/24"
+    gateway_id = aws_internet_gateway.custom_gw.id
+  }
+
+  tags = {
+    Name = "public"
+  }
+}
+
 resource "aws_subnet" "public_subnet" {
-  vpc_id            = aws_vpc.my_vpc.id 
+  vpc_id            = aws_vpc.custom_vpc.id 
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
-
+ 
 
 }
+
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public.id
+}
+
+
+resource "aws_security_group" "allow" {
+  name        = "allow"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.custom_vpc.id
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+  security_group_id = aws_security_group. allow.id
+  cidr_ipv4         = aws_vpc.main.cidr_block
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv6" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv6         = aws_vpc.main.ipv6_cidr_block
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv6         = "::/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 resource "aws_network_interface" "main" {
   subnet_id   = aws_subnet.public_subnet.id
@@ -49,12 +140,6 @@ resource "aws_instance" "linux" {
   credit_specification {
     cpu_credits = "unlimited"
   }
-}
-
- 
-resource "aws_internet_gateway_attachment" "main" {
-  internet_gateway_id = aws_internet_gateway.main.id
-  vpc_id              =  aws_vpc.my_vpc.id
 }
 
 
